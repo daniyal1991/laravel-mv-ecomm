3053b70f67ccabaa904129af840007db4376453f
Daniyal Khan <daniyal@deentek.com>
2023-05-15 01:07:55 +0500

    Admin seeder added

---
+++ database/seeders/InsertAdminsData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class InsertAdminsData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin_records = [
+            [
+                'id'=>1,
+                'name'=>'Super Admin',
+                'type'=>'superadmin',
+                'vendor_id'=>0,
+                'mobile'=>'1234567890',
+                'email'=>'superadmin@e.com',
+                'password'=>Hash::make('12345'),
+                'image'=>'',
+                'status'=>1
+            ]
+        ];
+
+        Admin::insert($admin_records);
+    }
+}
